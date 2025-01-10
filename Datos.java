57ae08fc53d41e3b5f4a136525d22ed742381731
juanEstebanBonilla <juanbonilla1@hotmail.com>
2025-01-10 12:01:04 -0500

    instroduccion java

---
+++ Datos.java
@@ -0,0 +1,12 @@
+public class Datos{
+    public static void main(String[]arg) {
+
+        System.out.println("Hola mundo");
+        int num1 = 2;
+	    double num2 = 2.6666;
+        num1=4;
+	    double operation =  num1 / num2;
+        System.out.println("Este es el numero " + num1);
+	    System.out.println("Este es el resultado:" + operation);
+    }
+}
